db6b5e572d36c7cba42da6b1e9f1707a846e9735
lolyparty <ololadeolayiwola01@gmail.com>
2020-07-03 00:59:54 +0100

    sliding navbar

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 
-   var closenav = function(){
-        document.querySelector('.hamburger-nav').classList.add('navbar-close');
-        document.querySelector('.hamburger-nav').classList.remove('navbtn');
-    }
+(function(){
+    document.querySelector('.hamburger').addEventListener('click',() =>{
+        document.querySelector('.nav-bar').classList.toggle('open');
+    });
+        
+        //nav bar
+        var list = document.getElementsByClassName('nav-link');
+        // console.log(list)
+        
+        for(var i = 0; i < list.length; i++){
+            list[i].addEventListener('click',()=>{
+                document.querySelector('.nav-bar').classList.remove('open');
 
-    var opennav = function(){
-        document.querySelector('.hamburger-nav').classList.add('navbtn');
-        document.querySelector('.hamburger-nav').classList.remove('navbar-close'); 
-    }   
-    
-    //nav bar
-    document.querySelector('.nav-button').addEventListener('click',opennav);
-    var list = document.getElementsByClassName('navside-link');
-    // console.log(list)
-    for(var i = 0; i < list.length; i++){
-        list[i].addEventListener('click',closenav);
-    }
-    document.querySelector('.closebtn').addEventListener('click',closenav);
+            });
+        }
+})();
 
+ 
 
+   
