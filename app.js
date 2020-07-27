a89ac1de09456a9c208b8f57f5c231f9adc68b71
lolyparty <ololadeolayiwola01@gmail.com>
2020-07-27 22:02:38 +0100

    scrolltop

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function(){    
+    
     document.querySelector('.hamburger').addEventListener('click',() =>{
         document.querySelector('.nav-bar').classList.toggle('open');
     });
@@ -13,7 +13,26 @@
 
             });
         }
-})();
+
+        let scrollHeight = Math.max(document.documentElement.offsetHeight,document.documentElement.scrollHeight, document.documentElement.clientHeight, document.body.scrollHeight, document.body.offsetHeight, document.body.clientHeight);
+        // console.log(scrollHeight)
+        
+        setInterval(()=>{
+          
+        var scrollPos = window.pageYOffset;
+        // console.log(scrollPos) 
+        
+        if(scrollPos > 3766){
+          document.querySelector('.scroll_top').style.display = 'block';
+        }else{
+          document.querySelector('.scroll_top').style.display = 'none'
+        }
+        }, 100)
+        
+        document.querySelector('.scroll_top').addEventListener('click', ()=>{
+          window.scrollTo(0,0)
+        })
+
 
  
 
